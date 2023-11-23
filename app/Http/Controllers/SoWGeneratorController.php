15c28fe4b83d2b8c153f4a822bdcdfdf56a42dd2
Raju at LHG <raju@lhgraphics.com>
2023-11-24 00:37:05 +0600

    Completed integrating apis for five steps

--- app/Http/Controllers/SoWGeneratorController.php
+++ app/Http/Controllers/SoWGeneratorController.php
@@ -13,6 +13,8 @@ class SoWGeneratorController extends Controller
 
     }
     public function generate(Request $request){
+
+        set_time_limit(500);
         $request->validate([
             'prompt' => 'required',
             'description' => 'required',
@@ -20,34 +22,93 @@ class SoWGeneratorController extends Controller
 
         $prompt = $request->input('prompt');
         $description = $request->input('description');
+        $transcript = preg_replace('/\d{2}:\d{2}\s/', '', $description);
 
         try {
-            $result = OpenAI::chat()->create([
-                // 'model' => 'gpt-4',
+
+            // Step One: Generate Summery from transcript
+            $summeryResult = OpenAI::chat()->create([
                 'model' => 'gpt-4-1106-preview',
                 'messages' => [
-                    ['role' => 'system', 'content' => 'You are a helpful system analyst assistan that generates a Scope of Work for projects and will always return output in markdown format.'],
-                    ['role' => 'user', 'content' => $prompt],
-                    ['role' => 'user', 'content' => $description],
+                    ['role' => 'system', 'content' => 'This is a transcript of a sales call with a potential new client for our other company, Defense Acquisition Solutions Group. Can you help me turn the transcript into a summary of the call/meeting and what we discussed? The format should include "Call Participants," which should be in bullet point format; "Meeting Summary," which should be in multiple paragraph format; and "Next Steps," which should be in bullet point format. Be sure to identify which person from the "Call Participants" is responsible for each "Next Steps" item and give some additional context and information in order to make sure each Next Step is clear for the person that needs to complete it.'],
+
+                    ['role' => 'system', 'content' => 'You will always return output in markdown format with proper line braeks.'],
+                    ['role' => 'user', 'content' => $transcript],
                 ],
-                'max_tokens' => 4000,
-                'temperature' => 0
+                'max_tokens' => 4096,
+                'temperature' => 0.5
             ]);
 
-            $sow = $result['choices'][0]['message']['content'];
-            \Log::info(['result' => $result]);
+            $summery = $summeryResult['choices'][0]['message']['content'];
+            \Log::info(['SummeryResult' => $summery]);
 
-            $sowObj = new ScopeOfWork();
+            // Step Two: Generate Problems and Goals from transcript
 
-            $sowObj->prompt = $prompt;
-            $sowObj->description = $description;
-            $sowObj->sow = $sow;
+            $problemsAndGoalsResult = OpenAI::chat()->create([
+                'model' => 'gpt-4-1106-preview',
+                'messages' => [
+                    ['role' => 'system', 'content' => "Help me turn the following transcript from a virtual meeting I had with a potential client into bullet points that address the client's problems and goals. The potential client is looking for help from our company, to help solve their problems and accomplish their goals with the services we offer."],
 
-            $sowObj->save();
+                    ['role' => 'system', 'content' => 'You will always return output in markdown format with proper line braeks.'],
+                    ['role' => 'user', 'content' => $transcript],
+                ],
+                'max_tokens' => 4096,
+                'temperature' => 0.5
+            ]);
 
-            return redirect()->route('sow.view', ['id' => $sowObj->id]);
+            $problemsAndGoals = $problemsAndGoalsResult['choices'][0]['message']['content'];
+            \Log::info(['problemsAndGoalsResult' => $problemsAndGoals]);
 
-            // return view('view', compact('sow'));
+            // Step Three: Generate Project Overview From Problems and Goals
+
+            $projectOverViewResult = OpenAI::chat()->create([
+                'model' => 'gpt-4-1106-preview',
+                'messages' => [
+                    ['role' => 'system', 'content' => "Help me take the following points that were put together from a conversation I had with a potential client asking for our businesses to help and turn them into paragraphs. I need this text to be a very easy-to-read, well-written, and detailed project description/overview. Put into paragraph format only that is easy to read."],
+
+                    ['role' => 'system', 'content' => 'I am sending you markdown and you will always return output in markdown format with proper line braeks with a heading Project Overview.'],
+                    ['role' => 'user', 'content' => $problemsAndGoals],
+                ],
+                'max_tokens' => 4096,
+                'temperature' => 0.5
+            ]);
+
+            $projectOverView = $projectOverViewResult['choices'][0]['message']['content'];
+            \Log::info(['projectOverViewResult' => $projectOverView]);
+
+            // Step Four: Generate Scope Of Work Bullet List From Problems and Goals
+            $scopeOfWorkResult = OpenAI::chat()->create([
+                'model' => 'gpt-4-1106-preview',
+                'messages' => [
+                    ['role' => 'system', 'content' => "I need your help in creating a very detailed bullet list for a scope of work based on the following Problems & Goals bullet list I created before. I need your help in making sure the new scope of work list you will be creating is very detailed and expanded upon as much as you can so we make sure nothing is missed for the project scope. I will end up using what you come up with in a proposal for a potential client who reached out to our company, asking us for help in the form of the services we offer. Be sure to add in quality control and testing items if not already mentioned in the list that I am providing you with. Please feel free to add the additional scope of work points that you think are missing and need to be added based on the main service the client is asking for our help with."],
+
+                    ['role' => 'system', 'content' => 'I am sending you markdown and you will always return output in markdown format with proper line braeks.'],
+                    ['role' => 'user', 'content' => $problemsAndGoals],
+                ],
+                'max_tokens' => 4096,
+                'temperature' => 0.5
+            ]);
+
+            $scopeOfWork = $scopeOfWorkResult['choices'][0]['message']['content'];
+            \Log::info(['scopeOfWorkResult' => $scopeOfWork]);
+
+            // Step Five: Generate deliverables for Scope of Work
+
+            $deliverablesResult = OpenAI::chat()->create([
+                'model' => 'gpt-4-1106-preview',
+                'messages' => [
+                    ['role' => 'system', 'content' => "I need your help in creating a very detailed bullet list for a scope of work based on the following Problems & Goals bullet list I created before. I need your help in making sure the new scope of work list you will be creating is very detailed and expanded upon as much as you can so we make sure nothing is missed for the project scope. I will end up using what you come up with in a proposal for a potential client who reached out to our company, asking us for help in the form of the services we offer. Be sure to add in quality control and testing items if not already mentioned in the list that I am providing you with. Please feel free to add the additional scope of work points that you think are missing and need to be added based on the main service the client is asking for our help with."],
+
+                    ['role' => 'system', 'content' => 'I am sending you markdown and you will always return output in markdown format with proper line braeks.'],
+                    ['role' => 'user', 'content' => $scopeOfWork],
+                ],
+                'max_tokens' => 4096,
+                'temperature' => 0.5
+            ]);
+
+            $deliverables = $deliverablesResult['choices'][0]['message']['content'];
+            \Log::info(['deliverablesResult' => $deliverables]);
+
         } catch (\Throwable $e) {
             throw $e;
             // Log the error for debugging and handle it appropriately.
