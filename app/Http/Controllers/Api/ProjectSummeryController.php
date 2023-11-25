9f26e232f600868f39bcce39a86e734216ead66f
Raju at LHG <raju@lhgraphics.com>
2023-11-26 02:01:42 +0600

    Sent Relational Data

--- app/Http/Controllers/Api/ProjectSummeryController.php
+++ app/Http/Controllers/Api/ProjectSummeryController.php
@@ -80,7 +80,7 @@ class ProjectSummeryController extends Controller
      * @urlParam id int Id of the transcript.
      */
     public function show($id){
-        $projectSummeryObj = ProjectSummary::with('meetingTranscript.problemsAndGoals')->find($id);
+        $projectSummeryObj = ProjectSummary::with(['meetingTranscript.problemsAndGoals.projectOverview', 'meetingTranscript.problemsAndGoals.scopeOfWork'])->find($id);
         $response = [
             'message' => 'Data Showed Successfully',
             'data' => $projectSummeryObj
