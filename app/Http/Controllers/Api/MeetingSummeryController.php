6195012ef5f05555cde6de8b34f9d3036fc1fce4
Raju at LHG <raju@lhgraphics.com>
2023-12-20 01:25:16 +0600

    Called tldv api

--- app/Http/Controllers/Api/MeetingSummeryController.php
+++ app/Http/Controllers/Api/MeetingSummeryController.php
@@ -8,6 +8,7 @@ use App\Models\MeetingSummery;
 use App\Services\OpenAIGeneratorService;
 use App\Services\PromptService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MeetingSummeryController extends Controller
 {
@@ -37,9 +38,12 @@ class MeetingSummeryController extends Controller
      * @bodyParam transcriptText string required The text of the transcript.
      * @bodyParam meetingName string required Name of the meeting.
      * @bodyParam meetingType integer required Meeting type [1: Client, 2: Intenal].
+     * @bodyParam clickupLink string Task url for the meeting.
+     * @bodyParam tldvLink string Tldv meeting url.
      */
 
     public function storeMeetingSummery(Request $request){
+        // return $this->getLastPartOfUrl($request->tldvLink);
         set_time_limit(500);
         $prompt = PromptService::findPromptByType($this->promptType);
         if($prompt == null){
@@ -50,10 +54,20 @@ class MeetingSummeryController extends Controller
             return response()->json($response, 422);
         }
         $validatedData = $request->validate([
-            'transcriptText' => 'required|string',
+            'clickupLink' => 'required|string',
+            'tldvLink' => 'nullable|string',
+            'transcriptText' => 'required_without:tldvLink,string',
             'meetingName' => 'required|string',
             'meetingType' => 'required|integer',
         ]);
+
+        if($request->filled('tldvLink')){
+            $meetingId = $this->getLastPartOfUrl($request->tldvLink);
+
+            if(isset($meetingId)){
+                $transcript = $this->getTldvTranscript($meetingId);
+            }
+        }
 
         // Generate Summery
         $summery = OpenAIGeneratorService::generateMeetingSummery($request->transcriptText, $prompt->prompt);
@@ -61,8 +75,10 @@ class MeetingSummeryController extends Controller
         $meetingSummeryObj = new MeetingSummery();
         $meetingSummeryObj->meetingName = $request->meetingName;
         $meetingSummeryObj->meetingType = $request->meetingType;
+        $meetingSummeryObj->tldvLink = $request->tldvLink;
+        $meetingSummeryObj->clickupLink = $request->clickupLink;
         $meetingSummeryObj->meetingSummeryText = $summery;
-        $meetingSummeryObj->transcriptText = $request->transcriptText;
+        $meetingSummeryObj->transcriptText = $transcript??$request->transcriptText;
 
         $meetingSummeryObj->save();
 
@@ -80,15 +96,19 @@ class MeetingSummeryController extends Controller
      * @group Meeting Summery
      *
      * @urlParam id int Id of the transcript.
-     * @bodyParam summaryText int required summaryText of the SOW Meeting Summery.
+     * @bodyParam transcriptText string required The text of the transcript.
      * @bodyParam meetingName string required Name of the meeting.
      * @bodyParam meetingType integer required Meeting type [1: Client, 2: Intenal].
+     * @bodyParam clickupLink string Task url for the meeting.
+     * @bodyParam tldvLink string Tldv meeting url.
      */
 
     public function updateMeetingSummery($id, Request $request){
-
         $validatedData = $request->validate([
             'summaryText' => 'required|string',
+            'clickupLink' => 'required|string',
+            'tldvLink' => 'nullable|string',
+            'transcriptText' => 'required_without:tldvLink,string',
             'meetingName' => 'required|string',
             'meetingType' => 'required|integer',
         ]);
@@ -98,6 +118,8 @@ class MeetingSummeryController extends Controller
         $meetingSummeryObj->meetingName = $request->meetingName;
         $meetingSummeryObj->meetingType = $request->meetingType;
         $meetingSummeryObj->transcriptText = $request->transcriptText;
+        $meetingSummeryObj->tldvLink = $request->tldvLink;
+        $meetingSummeryObj->clickupLink = $request->clickupLink;
 
         $meetingSummeryObj->save();
         $response = [
@@ -140,5 +162,45 @@ class MeetingSummeryController extends Controller
         ];
 
         return response()->json($response, 201);
+    }
+
+    private function getLastPartOfUrl($url){
+        // Parse the URL
+        $parsedUrl = parse_url($url);
+
+        // Get the path part
+        $path = isset($parsedUrl['path']) ? $parsedUrl['path'] : null;
+
+        // Get the last segment of the path
+        $lastSegment = basename($path);
+
+        return $lastSegment;
+    }
+
+    private function getTldvTranscript($id){
+        $apiUrl = 'https://pasta.tldv.io/v1alpha1/meetings/'.$id.'/transcript';
+        $apiKey = env('TLDV_API_KEY', 'd78e73c0-f2d8-468d-87c2-140518f98846');
+
+        try {
+            $response = Http::withHeaders([
+                'x-api-key' => $apiKey,
+                'Accept' => 'application/json',
+            ])->get($apiUrl);
+
+            // Check if the request was successful (status code 2xx)
+            if ($response->successful()) {
+                $data = $response->json(); // Get the response data
+                $transcript = '';
+                foreach ($data['data'] as $key => $content) {
+                    $transcript .= $content['speaker'] .': '. $content['text'].PHP_EOL;
+                }
+                return $transcript;
+            } else {
+                $errorMessage = $response->status() . ' ' . $response->reason();
+                return null;
+            }
+        } catch (\Exception $exception) {
+            return null;
+        }
     }
 }
