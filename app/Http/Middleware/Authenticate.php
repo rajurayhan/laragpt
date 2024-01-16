c4e9dec2a2d98782888cd7e108129892f8984e69
Raju Rayhan <raju.rayhan@yandex.com>
2024-01-17 02:34:16 +0600

    Revetrted auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,9 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated'], 401);
-        }
         return $request->expectsJson() ? null : route('login');
     }
 }
