b256ff52e105eb87fc6ab63d855eb03d4991a3f0
Raju at LHG <raju@lhgraphics.com>
2024-03-01 16:46:52 +0600

    Rectified Code

--- routes/web.php
+++ routes/web.php
@@ -91,45 +91,6 @@ Route::get('/order', function () {
     // $newItem = ["name" => "Service 5", "order" => 5];
     $orderManager->addOrUpdateItem($newItem);
 
-    // if(isset($newItem['id'])){
-    //     $service = Services::find($newItem['id']);
-
-    //     if($service->order > $newItem['order']){
-    //         $shiftRight = Services::where('order', '>=', $newItem['order'])->where('order', '<', $service->order)->get();
-    //         if($shiftRight){
-    //             foreach ($shiftRight as $key => $right) {
-    //                 $right->order = $right->order+1;
-    //                 $right->save();
-    //             }
-    //         }
-    //     }
-    //     else{
-    //         $shiftLeft = Services::where('order', '<=', $newItem['order'])->where('order', '>', $service->order)->get();
-    //         if($shiftLeft){
-    //             foreach ($shiftLeft as $key => $left) {
-    //                 $left->order = $left->order-1;
-    //                 $left->save();
-    //             }
-    //         }
-    //     }
-
-    //     $service->order = $newItem['order'];
-    //     $service->save();
-    // }
-
-    // else{
-    //     $existingData = Services::where('order', '>=', $newItem['order'])->get();
-
-    //     if($existingData){
-    //         foreach ($existingData as $key => $exist) {
-    //             $exist->order = $exist->order+1;
-    //             $exist->save();
-    //         }
-    //     }
-
-    //     Services::create($newItem);
-    // }
-
     $data = Services::orderBy('order')->get();
 
     return response()->json($data);
