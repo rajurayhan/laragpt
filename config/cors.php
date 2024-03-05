825ad3daaa85f5969a10e7de9d4c1da2aab69826
Raju at LHG <raju@lhgraphics.com>
2024-03-05 06:03:44 +0600

    Enabled CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'http://localhost:3000'],
+    'allowed_origins' => ['*', 'http://localhost:3000', 'https://lhgnewv2.flywheelstaging.com', 'https://lhgraphics.com'],
 
     'allowed_origins_patterns' => [],
 
