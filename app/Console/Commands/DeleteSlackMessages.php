04afa322cab60fcca4ad8a39b9072933ae104936
Raju at LHG <raju@lhgraphics.com>
2024-03-08 04:33:19 +0600

    Added Slack Message deleter

---
+++ app/Console/Commands/DeleteSlackMessages.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class DeleteSlackMessages extends Command
+{
+    protected $signature = 'slack:delete-messages';
+    protected $description = 'Delete Slack messages within a date range';
+
+    public function handle()
+    {
+        $slackToken = 'xoxp-8666569908-1216297276278-6750628571495-00d30c110255379f837a8455f1d7de02';
+
+        $channelId = 'C067C7YLA7Q';
+
+        // Specify the date range
+        $startDate = strtotime('2024-03-07');
+        $endDate = strtotime('2024-03-08');
+
+        // Convert dates to Slack timestamp format
+        $startTimestamp = date('Y-m-d H:i:s', $startDate);
+        $endTimestamp = date('Y-m-d H:i:s', $endDate);
+
+        // Retrieve messages in the date range
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $slackToken,
+        ])->get('https://slack.com/api/conversations.history', [
+            'channel' => $channelId,
+            'limit' => 1000,
+            'oldest' => strtotime($startTimestamp),
+            'latest' => strtotime($endTimestamp),
+        ]);
+
+        $messages = $response->json()['messages'];
+
+        // Delete each message
+        $i= 1;
+        foreach ($messages as $index => $message) {
+            $timestamp = $message['ts'];
+            if ($message['text'] === ".") {
+                $deleteResponse = Http::withHeaders([
+                    'Authorization' => 'Bearer ' . $slackToken,
+                ])->post('https://slack.com/api/chat.delete', [
+                    'channel' => $channelId,
+                    'ts' => $timestamp,
+                ]);
+                $this->info($i++.' - '.$message['text'] . ' => Deleted ? ' .$deleteResponse->json()["ok"]);
+
+                \Log::info([$message['ts'] => $deleteResponse->json()["ok"]]);
+
+                if (($index + 1) % 100 === 0) {
+                    $this->info("Waiting for 1 minutes...");
+                    sleep(60); // Sleep for 1 minutes (60 seconds)
+                }
+            }
+        }
+
+        $this->info('Slack messages deleted successfully.');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')->everyTwoMinutes();
+        $schedule->command('slack:delete-messages')->everyMinute();
     }
 
     /**
