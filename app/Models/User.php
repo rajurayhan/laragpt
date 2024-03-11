bbe15e4c85c6fa6a4e8abd5e6475b1bf38bad0c4
Raju at LHG <raju@lhgraphics.com>
2024-03-11 23:43:35 +0600

    Set default guard name

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+    protected function getDefaultGuardName(): string { return 'sanctum'; }
+
     /**
      * The attributes that are mass assignable.
      *
