35279ab6fe824f9814be88dcfb183eb7014dae63
Raju at LHG <raju@lhgraphics.com>
2024-03-15 00:18:10 +0600

    Added default order

---
+++ app/Console/Commands/SetDefaultOrderOnServiceModule.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\ServiceDeliverables;
+use App\Models\ServiceDeliverableTasks;
+use App\Models\ServiceGroups;
+use App\Models\Services;
+use App\Models\ServiceScopes;
+use Illuminate\Console\Command;
+
+class SetDefaultOrderOnServiceModule extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'service:set-order';
+    protected $description = 'Set a default order on service module data';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $services = Services::get();
+        foreach ($services as $key => $service) {
+            $service->order = $key+1;
+            $service->save();
+        }
+
+        $serviceGroup = ServiceGroups::get();
+        foreach ($serviceGroup as $key => $group) {
+            $group->order = $key+1;
+            $group->save();
+        }
+
+        $serviceScope = ServiceScopes::get();
+        foreach ($serviceScope as $key => $scope) {
+            $scope->order = $key+1;
+            $scope->save();
+        }
+
+        $serviceDeliverables = ServiceDeliverables::get();
+        foreach ($serviceDeliverables as $key => $deliverable) {
+            $deliverable->order = $key+1;
+            $deliverable->save();
+        }
+
+        $serviceDeliverableTasks = ServiceDeliverableTasks::get();
+        foreach ($serviceDeliverableTasks as $key => $task) {
+            $task->order = $key+1;
+            $task->save();
+        }
+
+        $this->info('Order Set Successfully');
+
+    }
+}
