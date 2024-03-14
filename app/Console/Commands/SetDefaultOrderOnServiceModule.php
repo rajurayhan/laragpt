fdf1bd9571848e751a87405fefa3a53440b4b66d
Raju at LHG <raju@lhgraphics.com>
2024-03-15 02:05:04 +0600

    Updated order script

--- app/Console/Commands/SetDefaultOrderOnServiceModule.php
+++ app/Console/Commands/SetDefaultOrderOnServiceModule.php
@@ -38,6 +38,7 @@ class SetDefaultOrderOnServiceModule extends Command
         }
         foreach($filteredData as $dataset){
             foreach ($dataset as $key => $data) {
+                $data->order = $key+1;
                 $data->save();
             }
         }
@@ -50,6 +51,7 @@ class SetDefaultOrderOnServiceModule extends Command
         }
         foreach($filteredData as $dataset){
             foreach ($dataset as $key => $data) {
+                $data->order = $key+1;
                 $data->save();
             }
         }
@@ -62,6 +64,7 @@ class SetDefaultOrderOnServiceModule extends Command
         }
         foreach($filteredData as $dataset){
             foreach ($dataset as $key => $data) {
+                $data->order = $key+1;
                 $data->save();
             }
         }
@@ -74,6 +77,7 @@ class SetDefaultOrderOnServiceModule extends Command
         }
         foreach($filteredData as $dataset){
             foreach ($dataset as $key => $data) {
+                $data->order = $key+1;
                 $data->save();
             }
         }
